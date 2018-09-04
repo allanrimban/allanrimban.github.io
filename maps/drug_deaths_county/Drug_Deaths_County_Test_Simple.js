c4ddb4c6574055444262d003f9a26fcbafdd6ab6
Allan Mark D. Rimban <allanrimban@gmail.com>
2018-09-04 01:27:34 -0700

    Update Drug_Deaths_County_Test_Simple.js

--- maps/drug_deaths_county/Drug_Deaths_County_Test_Simple.js
+++ maps/drug_deaths_county/Drug_Deaths_County_Test_Simple.js
@@ -1,9 +1,5 @@
-var drugDeaths = {
-"type": "FeatureCollection",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "GEOID": "6075", "LONG": -122.4397, "LAT": 37.7567, "County": "San Francisco County, CA", "Deaths": 196, "Population": 870887, "Crude Rate": "22.5" }, "geometry": { "type": "Point", "coordinates": [ -122.43967004828126, 37.756680048364046 ] } },
+var drugDeaths = [{ "type": "Feature", "properties": { "GEOID": "6075", "LONG": -122.4397, "LAT": 37.7567, "County": "San Francisco County, CA", "Deaths": 196, "Population": 870887, "Crude Rate": "22.5" }, "geometry": { "type": "Point", "coordinates": [ -122.43967004828126, 37.756680048364046 ] } },
 { "type": "Feature", "properties": { "GEOID": "5145", "LONG": -91.7454, "LAT": 35.256, "County": "White County, AR", "Deaths": 13, "Population": 79263, "Crude Rate": "Unreliable" }, "geometry": { "type": "Point", "coordinates": [ -91.745435689016304, 35.256004631574996 ] } },
 { "type": "Feature", "properties": { "GEOID": "39005", "LONG": -82.2711, "LAT": 40.846, "County": "Ashland County, OH", "Deaths": 13, "Population": 53652, "Crude Rate": "Unreliable" }, "geometry": { "type": "Point", "coordinates": [ -82.271065950662205, 40.846044901603271 ] } }
 ]
-}
+
